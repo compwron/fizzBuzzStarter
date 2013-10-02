32909554b8f276f47bdc52c9ba9a76016a3b370e
compwron <wordymusic@gmail.com>
2013-10-01 20:04:13 -0700

    Working main for ruby version

---
+++ ruby/bin/go.rb
@@ -0,0 +1,3 @@
+require_relative "../lib/fizz_buzz"
+
+puts FizzBuzz.new(100).numbers.join("\n")
--- ruby/lib/fizz_buzz.rb
+++ ruby/lib/fizz_buzz.rb
@@ -1,5 +1,11 @@
 class FizzBuzz
-  # attr_reader :numbers
+  attr_reader :numbers
+
+  def initialize count
+  	@numbers = (1..count).map{ |number|
+  		value_of(number)
+  	}
+  end
 
   def value_of number
   	return (number % 15 == 0) ? "FizzBuzz" : (number % 3 == 0 ? "Fizz" : (number % 5 == 0 ? "Buzz" : number.to_s))
