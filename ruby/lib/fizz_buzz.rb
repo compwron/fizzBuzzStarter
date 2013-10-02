713c8e613ffa527ef1d05f1995e97599e0d99cea
compwron <wordymusic@gmail.com>
2013-10-01 19:42:51 -0700

    Passing test for Buzz

--- ruby/lib/fizz_buzz.rb
+++ ruby/lib/fizz_buzz.rb
@@ -2,6 +2,8 @@ class FizzBuzz
   # attr_reader :numbers
 
   def value_of number
-    number % 3 == 0 ? "Fizz" : number.to_s
+    if (number % 3 == 0) then return "Fizz" end
+   	if (number % 5 == 0) then return "Buzz" end
+   	number.to_s
   end
 end
