f4e88a1fbaa80ec03e0308c3cb62cdd0cf790944
compwron <wordymusic@gmail.com>
2013-10-01 20:12:16 -0700

    Java: passing test for 5 = Buzz

--- java/FizzBuzz/src/FizzBuzz.java
+++ java/FizzBuzz/src/FizzBuzz.java
@@ -3,6 +3,8 @@ public class FizzBuzz {
     public String valueOf(int number) {
         if (number % 3 == 0) {
             return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
         } else {
             return String.valueOf(number);
         }
