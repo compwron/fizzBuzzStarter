16f02d28a9b766354f27f8f3bae2d86e85295c55
compwron <wordymusic@gmail.com>
2013-10-01 19:32:36 -0700

    Failing test for 2

--- ruby/spec/fizz_buzz_spec.rb
+++ ruby/spec/fizz_buzz_spec.rb
@@ -6,5 +6,9 @@ describe FizzBuzz do
     it "should return 1 for 1" do
       FizzBuzz.new.value_of(1).should == "1"
     end
+
+    it "should return 2 for 2" do
+      FizzBuzz.new.value_of(2).should == "2"
+    end
   end
 end
