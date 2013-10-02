6075628127824cc4a3a13bc55d4f4e88c7e606ab
compwron <wordymusic@gmail.com>
2013-10-01 19:44:12 -0700

    Passing test for 15 = FizzBuzz; needs refactoring

--- ruby/lib/fizz_buzz.rb
+++ ruby/lib/fizz_buzz.rb
@@ -2,6 +2,7 @@ class FizzBuzz
   # attr_reader :numbers
 
   def value_of number
+  	if (number % 15 == 0) then return "FizzBuzz" end
     if (number % 3 == 0) then return "Fizz" end
    	if (number % 5 == 0) then return "Buzz" end
    	number.to_s
